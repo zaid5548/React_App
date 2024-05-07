0cea64c4b06d915248e219e44d692e60fca1573b
Zaid Khan <zaidkhan5548@gmail.com>
2024-05-07 15:09:32 +0530

    basic react project

---
+++ my-first-app/src/App.js
@@ -0,0 +1,12 @@
+import Test from './Test'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React ... </h1>
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-app/src/Test.js
@@ -0,0 +1,7 @@
+function Test(){
+    return (
+        <h1>Hello Testing Component !</h1>
+    );
+}
+
+export default Test;
